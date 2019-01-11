1379a4b3584f2f9f25da61db1fcd017939972440
Hai Nguyen <hai.nguyen.pv12@gmail.com>
2019-01-11 12:14:23 +0700

    Create Dung.cs

---
+++ ProjectTest/Dung.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTest
+{
+    class Dung
+    {
+        public void DMethod()
+        {
+            Console.WriteLine("Hello Dung");
+        }
+    }
+}
