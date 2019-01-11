7a70c4fa0da4367434839373179fadbccdcfacc0
Hai Nguyen <hai.nguyen.pv12@gmail.com>
2019-01-11 12:07:17 +0700

    initial commit

---
+++ ProjectTest/Thien.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTest
+{
+    class Thien
+    {
+        public void TMethod()
+        {
+            Console.WriteLine("Hello Thien");
+        }
+    }
+}
